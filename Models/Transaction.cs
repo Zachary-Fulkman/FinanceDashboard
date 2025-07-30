9f0c2c03c5e50eea419aa93b2f660d39cc957df0
Zachary-Fulkman <163482286+Zachary-Fulkman@users.noreply.github.com>
2025-07-30 09:33:41 -0400

    Added Transaction and User models

---
+++ Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿namespace FinanceDashboard.API.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public bool IsIncome { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FinanceDashboard.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
